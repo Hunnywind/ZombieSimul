705933095255027a2f0e3f66fad8eafccda5eef8
pmk4545 <hjp9595@naver.com>
2017-11-04 22:50:29 +0900

    하트모양 표시

--- Assets/Scripts/UI_control.cs
+++ Assets/Scripts/UI_control.cs
@@ -125,13 +125,13 @@ public class UI_control : MonoBehaviour {
         int hun = int.Parse(hunger);
         int lif = int.Parse(life);
 
-        if (hun > lov && hun > 0)
+        if (hun >= lov && hun > 0)
         {
             _iconTemps[num] = 1;
             //_answerIcons[num].gameObject.SetActive(true);
             //_answerIcons[num].sprite = _answerIconSprites[0];
         }
-        else if (lov > hun && hun > 0)
+        else if (lov > hun && lov > 0)
         {
             _iconTemps[num] = 0;
             //_answerIcons[num].gameObject.SetActive(true);
