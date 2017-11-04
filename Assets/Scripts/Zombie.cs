5d0d4cfa8158c5a7348d576cff3f2b5399ae533e
DESKTOP-7UJHNSB\theki <thekinggun@gmail.com>
2017-11-04 09:16:05 +0900

    temp

---
+++ Assets/Scripts/Zombie.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zombie : MonoBehaviour {
+    public int _love { get; private set; }
+    public int _hungry { get; private set; }
+    public int _life { get; private set; }
+}
