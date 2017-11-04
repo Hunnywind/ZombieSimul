80ea57e077e350746b03a81baa2fe710bf402bdc
DESKTOP-7UJHNSB\theki <thekinggun@gmail.com>
2017-11-04 10:58:22 +0900

    csv file modify

--- Assets/Scripts/Manager/DialogMng.cs
+++ Assets/Scripts/Manager/DialogMng.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
 
 public class DialogMng : Singleton<DialogMng> {
@@ -8,33 +9,46 @@ public class DialogMng : Singleton<DialogMng> {
     [SerializeField]
     private Zombie _zombie;
 
+    private Dictionary<int, Dictionary<string, string>> _qData = new Dictionary<int, Dictionary<string, string>>();
+    private Dictionary<int, Dictionary<string, string>> _aData = new Dictionary<int, Dictionary<string, string>>();
+
+    void Start()
+    {
+        Init();
+    }
+
     void Init()
     {
+        _qData = GameData.GetInstance.GetData("Question");
+        _aData = GameData.GetInstance.GetData("Answer");
+        ShowDialog();
     }
     public void ShowDialog()
     {
-        var Qdata = GameData.GetInstance.GetData("Question");
-        var Adata = GameData.GetInstance.GetData("Answer");
-        var ranValue = Random.Range(0, Qdata.Count);
+        Stopwatch sw = new Stopwatch();
+        sw.Start();
+        var ranValue = Random.Range(0, _qData.Count);
         while (true)
         {
-            ranValue = Random.Range(0, Qdata.Count);
-            var d = int.Parse(Qdata[ranValue]["L_Condition"]);
+            ranValue = Random.Range(0, _qData.Count);
+            var d = int.Parse(_qData[ranValue]["L_Condition"]);
             if (_zombie._love >= d)
             {
-                _uiControl.SetTalk(Qdata[ranValue]["Question"]);
+                _uiControl.SetTalk(_qData[ranValue]["Question"]);
                 break;
             }
         }
         int buttonNum = 0;
-        for (int i = 0; i < Adata.Count; i++)
+        for (int i = 0; i < _aData.Count; i++)
         {
-            if (Adata[i]["Qid"].Equals(ranValue.ToString()))
+            if (_aData[i]["Qid"].Equals(ranValue.ToString()))
             {
-                _uiControl.SetAnswer(buttonNum, Adata[i]["Answer"]);
+                _uiControl.SetAnswer(buttonNum, _aData[i]["Answer"]);
                 buttonNum++;
             }
         }
+        sw.Stop();
+        UnityEngine.Debug.Log(sw.ElapsedMilliseconds.ToString() + "ms");
     }
 
 }
